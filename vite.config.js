164b6d1e84de5e6a34e8e93ca1a1ecd59f0131f5
cmedinilla <cris.medinill@gmail.com>
2025-01-31 10:59:43 -0600

    Add URL base

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
-const URL_BASE = process.env.URL_BASE || '/';
+const URL_BASE = process.env.URL_BASE || '/viviendoconcuci-landing-page/';
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
